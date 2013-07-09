24fc933e3461018e71e225129c2c02c50be7b0f3
Take2pm <ubuntu@ip-172-31-17-170.us-west-2.compute.internal>
2013-07-09 21:29:36 +0000

    alllowed grader to read file from url

--- grader.js
+++ grader.js
@@ -5,6 +5,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var rest = require('../restler');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -14,6 +15,20 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+
+var assertUrlExists = function(url) {
+	var fileUrl = url.toString();
+	var content;
+	rest.get(fileUrl).on('complete', function(result) {
+		if (result instanceof Error) {
+			console.log("%s could not retrieve data. Exiting.", fileUrl);
+			process.exit(1);
+		}
+		content = result;
+	});
+	return content;
+};
+					
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -25,14 +40,18 @@ var loadChecks = function(checksfile) {
 
 var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
+    return doCheck($, checksfile);
+};
+
+var doCheck = function(cheerio, checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
+        var present = cheerio(checks[ii]).length > 0;
         out[checks[ii]] = present;
     }
     return out;
-};
+}
 
 var clone = function(fn) {
     // Workaround for commander.js issue.
@@ -41,13 +60,27 @@ var clone = function(fn) {
 };
 
 if(require.main == module) {
-    program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+	program
+		.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+		.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+		.option('-u, --url <url>', 'Url to index.html')
+		.parse(process.argv);
+
+	if (program.url) {
+		rest.get(program.url).on('complete', function(result) {
+				if (result instanceof Error) {
+					console.log("%s could not retrieve data. Exiting.", program.url);
+					process.exit(1);
+				}
+				var checkJson = doCheck(cheerio.load(result), program.checks);
+				var outJson = JSON.stringify(checkJson, null, 4);
+				console.log(outJson);
+			});
+	} else {
+		var checkJson = checkHtmlFile(program.file, program.checks);
+		var outJson = JSON.stringify(checkJson, null, 4);
+		console.log(outJson);
+	}
 } else {
-    exports.checkHtmlFile = checkHtmlFile;
+	exports.checkHtmlFile = checkHtmlFile;
 }
